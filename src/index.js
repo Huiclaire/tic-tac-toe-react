67cd13663efda0ccd0cfd03e2397a661ecf3acff
Claire Lin <chunhuilin7516@gmail.com>
2024-02-02 15:33:00 +0100

    building the board

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+// the bridge between the component created in App.js and the browser.
 import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client"; // React DOM: React's library to talk to web browsers
 import "./styles.css";
 
 import App from "./App";
