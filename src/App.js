7557416c5ae0feb9530e0aeb0c3b8225820c41c0
Claire Lin <chunhuilin7516@gmail.com>
2024-02-27 19:59:21 +0100

    Add simple design

--- src/App.js
+++ src/App.js
@@ -94,19 +94,16 @@ export default function Board() {
 
   return (
     <>
+      <h1>Tic Tac Toe Game</h1>
       <div className="status">{status}</div>
-      <div className="board-row">
+      <div class="grid">
         {/* when the square is clicked, the code after the arrow will run, calling 'handleclick(0) */}
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
         <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
         <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
         <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
